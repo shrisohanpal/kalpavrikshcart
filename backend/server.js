678572f723fbc9e5965790978328021f28d327a7
bhola kumar <bhola870048@gmail.com>
2021-02-23 01:25:50 +0530

    Second Commit

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
@@ -12,12 +13,24 @@ connectDB()
 
 const app = express()
 
-app.get('/', (req, res) =>
-{
-    res.send('API is running!')
-})
-
 app.use('/api/products', productRoutes)
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    )
+} else {
+    app.get('/', (req, res) =>
+    {
+        res.send('API is running....')
+    })
+}
+
 
 app.use(notFound)
 
